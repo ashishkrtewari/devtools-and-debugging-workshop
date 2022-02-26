080927a9d60723834b9d03209389b7b6d9a24af6
Ashish Kumar Tewari <ashishtewaripro@gmail.com>
2022-02-26 16:24:52 +0530

    Added Agenda

---
+++ logs.js
@@ -0,0 +1,16 @@
+const infos = () => {
+  console.info("Welcome to DevTools and Debugging Workshop");
+};
+const warnings = () => {
+  console.warn("Keep Learning");
+  console.warn("Its getting boring");
+};
+const errors = () => {
+  console.error("No Student Found");
+  console.error("No Mentor Found");
+};
+const renderLogs = () => {
+  infos();
+  warnings();
+  errors();
+};
