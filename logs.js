c7ad32009a2c14fd66762961cc1e8f2c526e543b
Ashish Kumar Tewari <ashishtewaripro@gmail.com>
2022-02-27 20:44:03 +0530

    added more scope variables

--- logs.js
+++ logs.js
@@ -1,13 +1,18 @@
+const user = {};
 const infos = () => {
+  const count = 10;
   console.info("Welcome to DevTools and Debugging Workshop");
+  user.name = "Ashish";
 };
 const warnings = () => {
   console.warn("Keep Learning");
   console.warn("Its getting boring");
+  user.country = "India";
 };
 const errors = () => {
   console.error("No Student Found");
   console.error("No Mentor Found");
+  user.hobby = "singing";
 };
 const renderLogs = () => {
   infos();
